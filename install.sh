87bc98667753ab18a2cbb223ec634e8a3d173341
Andrew Alexander <adrw@squareup.com>
2020-10-04 22:45:28 -0400

    Add webp2jpeg

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 touch cmd-all-files && rm cmd-all-files
+touch webp2jpeg && rm webp2jpeg
 curl -s https://raw.githubusercontent.com/adrw/cmd-all-files/main/bin/cmd-all-files -o cmd-all-files
+curl -s https://raw.githubusercontent.com/adrw/cmd-all-files/main/bin/webp2jpeg -o webp2jpeg
 chmod +x cmd-all-files
+chmod +x webp2jpeg
 mv cmd-all-files /usr/local/bin/
+mv webp2jpeg /usr/local/bin/
