870c673e0907510b8761661216e926c1cf137706
Andrew Alexander <adrw@squareup.com>
2020-10-04 22:27:13 -0400

    Fix install

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-rm cmd-all-files
+touch cmd-all-files && rm cmd-all-files
 curl -s https://raw.githubusercontent.com/adrw/cmd-all-files/main/bin/cmd-all-files -o cmd-all-files
 chmod +x cmd-all-files
 mv cmd-all-files /usr/local/bin/
